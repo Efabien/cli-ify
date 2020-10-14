672d9f1230ff5bdc049a7ef14b150522cff7a01f
Efabien <fabien@topteam.tech>
2020-10-14 16:00:11 +0300

    wip

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const logger = require('../lib/logger');
 
 const config = yaml.safeLoad(fs.readFileSync('./cli-ify.yml', 'utf8'));
-
 module.exports = config;
