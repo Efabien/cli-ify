2e6029b95ed89b46c005346c919c31f22776dcf5
Efabien <fabien@topteam.tech>
2020-11-16 09:16:09 +0300

    exit with code 1 on error

--- example/modules/ex1.js
+++ example/modules/ex1.js
@@ -2,6 +2,7 @@ const func = (arg, { logger, spinnerFactory, dependencies }) => {
   dependencies.log('foo', logger)
   const spinner = spinnerFactory.create('wait');
   spinner.start();
+  throw Error('foo error');
   logger.data(arg);
   spinner.succeed();
 };
