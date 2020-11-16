39f38b584092437da827a1b3fdad20a6f79506be
Efabien <fabien@topteam.tech>
2020-11-16 11:29:10 +0300

    add manifest validation

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const colors = require('colors');
 
 const yargs = require('yargs');
 const callsite = require('callsite');
+const Joi = require('joi');
 
 const {
   registerCommands, 
   registerOptions
 } = require('./lib/init-yargs');
+
+const {
+  settingsSchema,
+  commandsSchema,
+  optionsSchema
+} = require('./joi-schemas/schemas');
 
 class CliIfy {
   constructor(configPath) {
@@ -18,36 +25,60 @@ class CliIfy {
     this._options;
     this._settings;
     this._yargs = yargs;
-    this._execDir;
+    this._manifestSchema = Joi.compile({
+      settings: settingsSchema,
+      commands: commandsSchema,
+      options: optionsSchema
+    });
   }
 
   init({ manifest, dependencies }) {
     try {
-      this._setExecDir();
+      const execDir = this._getExecDir();
       const {
         commands,
         options,
         settings
-      }  = yaml.safeLoad(
-        fs.readFileSync(path.resolve(this._execDir, manifest), 'utf8')
-      );
+      }  = this._loadConfig(execDir, manifest);
       this._commands = commands;
       this._options = options;
       this._settings = settings;
-      registerCommands(this._commands, this._yargs, this._settings, this._execDir, dependencies);
+      registerCommands(this._commands, this._yargs, this._settings, execDir, dependencies);
       registerOptions(this._options, this._yargs, this._settings);
       this._yargs.argv;
     } catch (e) {
       if (e.name && e.name === 'YAMLException') {
-        return console.log(colors.red(`YAMLException : ${e.message}`));
+        return console.log(colors.red(`${e.name} : ${e.message}`));
       }
+
+      if (e.name && e.name === 'ManifestException') {
+        return console.log(colors.red(e));
+      }
+      return console.log(e)
     }
   }
 
-  _setExecDir() {
+  _loadConfig(execDir, manifest) {
+    const configs = yaml.safeLoad(
+      fs.readFileSync(path.resolve(execDir, manifest), 'utf8')
+    );
+    return this._validateSchema(configs);
+  }
+
+  _validateSchema(configs) {
+    const result = this._manifestSchema.validate(configs);
+    if (result.error) {
+      const err = new Error(result.error.details[0].message);
+      err.name = 'ManifestException';
+      throw err;
+    }
+    return result.value;
+  }
+
+  _getExecDir() {
     const stack = callsite();
     const requester = stack[2].getFileName();
-    this._execDir = path.dirname(requester);
+    return path.dirname(requester);
   }
 };
 
---
+++ joi-schemas/schemas.js
@@ -0,0 +1,64 @@
+const Joi = require('joi');
+
+const settingsSchema = Joi.object({
+  interactivity: Joi.object({
+    label: Joi.string().required(),
+    delimiter: Joi.string().required()
+  }).required(),
+  theme: Joi.object({
+    silly: Joi.string().required(),
+    input: Joi.string().required(),
+    verbose: Joi.string().required(),
+    prompt: Joi.string().required(),
+    info: Joi.string().required(),
+    data: Joi.string().required(),
+    help: Joi.string().required(),
+    warn: Joi.string().required(),
+    debug: Joi.string().required(),
+    error: Joi.string().required()
+  }).required()
+}).required();
+
+const subCommands = Joi.array().items(
+  Joi.object().pattern(
+    /^/,
+    Joi.object({
+      describe: Joi.string().required(),
+      ['sub-module']: Joi.string().required()
+    }).required()
+  ).required()
+).optional();
+
+const prompts = Joi.array().items(Joi.object().pattern(
+  /^/,
+  Joi.object({
+    description: Joi.string().required()
+  }).required()
+).required()).optional();
+
+const commandsSchema = Joi.array().items(Joi.object().pattern(
+  /^/,
+  Joi.object({
+    describe: Joi.string().required(),
+    module: Joi.string().required(),
+    ['sub-commands']: subCommands,
+    prompts,
+    argv: Joi.string().optional()
+  }).required()
+  ).required())
+.required();
+
+const optionsSchema = Joi.array().items(Joi.object().pattern(
+  /^/,
+  Joi.object({
+    alias: Joi.string().required(),
+    type: Joi.string().valid('boolean', 'string', 'number').required(),
+    description: Joi.string().required()
+  }).required()
+).required()).optional();
+
+module.exports = {
+  settingsSchema,
+  commandsSchema,
+  optionsSchema
+};
